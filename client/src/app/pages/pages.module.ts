1ec65c6739d54576d63aec03b59aca91b1172c23
atrathbone <alexander.thomas.rathbone@hotmail.com>
2022-07-18 22:45:59 +0200

    add rudimentary styling and images

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -6,10 +6,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateCardComponent } from './create-card/create-card.component';
 import { BrowseCardsComponent } from './browse-cards/browse-cards.component';
 import { CollectionsComponent } from './collections/collections.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AuthComponent, DashboardComponent, CreateCardComponent, BrowseCardsComponent, CollectionsComponent],
-  imports: [CommonModule, ComponentsModule],
+  declarations: [
+    AuthComponent,
+    DashboardComponent,
+    CreateCardComponent,
+    BrowseCardsComponent,
+    CollectionsComponent,
+  ],
+  imports: [CommonModule, ComponentsModule, MatCardModule],
   exports: [AuthComponent],
 })
 export class PagesModule {}
