34ec26b124e9d48985aadb5f1987bd3c3497ead6
atrathbone <alexander.thomas.rathbone@hotmail.com>
2022-07-18 20:19:30 +0200

    add service to create pages

---
+++ backend/src/card/card-archive/card-archive.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@nestjs/common';
+import Jimp from 'jimp/*';
+import { CardService } from '../card.service';
+import { ImageManipulationService } from '../image-manipulation/image-manipulation.service';
+
+@Injectable()
+export class CardArchiveService {
+  constructor(
+    private imageManipulator: ImageManipulationService,
+    private cardService: CardService,
+  ) {}
+
+  public generatePageImage(cards: Jimp[]): Promise<Jimp> {
+    return new Promise((resolve, reject) => {
+      this.imageManipulator
+        .loadFromPath('src/image-processing-assets/blank.jpg')
+        .then((background) => {
+          this.imageManipulator
+            .overlay(background, cards[0], 15, 15)
+            .then((withOne) => {
+              this.imageManipulator
+                .overlay(withOne, cards[1], 868, 15)
+                .then((withTwo) => {
+                  this.imageManipulator
+                    .overlay(withTwo, cards[2], 1721, 15)
+                    .then((withThree) => {
+                      this.imageManipulator
+                        .overlay(withThree, cards[3], 15, 1234)
+                        .then((withFour) => {
+                          this.imageManipulator
+                            .overlay(withFour, cards[4], 868, 1234)
+                            .then((withFive) => {
+                              this.imageManipulator
+                                .overlay(withFive, cards[5], 1721, 1234)
+                                .then((withSix) => {
+                                  this.imageManipulator
+                                    .overlay(withSix, cards[6], 15, 2454)
+                                    .then((withSeven) => {
+                                      this.imageManipulator
+                                        .overlay(withSeven, cards[7], 868, 2454)
+                                        .then((withEight) => {
+                                          this.imageManipulator
+                                            .overlay(
+                                              withEight,
+                                              cards[8],
+                                              1721,
+                                              2454,
+                                            )
+                                            .then((page) => {
+                                              resolve(page);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  public async generatePdf(cardIds: string[]) {
+    let idPages: any[] = [];
+    let pages: Jimp[] = [];
+    for (let i = 0; i < cardIds.length; i += 9) {
+      const page = cardIds.slice(i, i + 9);
+      idPages.push(page);
+    }
+    for (let page of idPages) {
+      const images = await this.getCardImages(page);
+      const pageImg = await this.generatePageImage(images);
+      pages.push(pageImg);
+    }
+    //save images and put them in a pdf
+  }
+
+  public async getCardImages(cardIds: string[]) {
+    const paths = (await this.cardService.populateCards(cardIds)).map(
+      (card) => {
+        return card.imageUrl;
+      },
+    );
+    const images: Jimp[] = [];
+    for (let path of paths) {
+      const img = await this.imageManipulator.loadFromPath(path);
+      images.push(img);
+    }
+    if (images.length < 9) {
+      let blanks = 9 - images.length;
+      for (let i = 0; i < blanks; i++) {
+        const blank = await this.imageManipulator.loadFromPath(
+          'src/image-processing-assets/blank-card.png',
+        );
+        images.push(blank);
+      }
+    }
+    return images;
+  }
+}
